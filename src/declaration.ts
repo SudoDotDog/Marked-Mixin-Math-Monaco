de6be7303769b49cbf8c32fb3bb8a09c50f0185f
wmxpy <wm@mengw.io>
2022-09-18 22:43:55 -0500

    add base mixin

--- src/declaration.ts
+++ src/declaration.ts
@@ -9,4 +9,21 @@ import { New_Line_Character } from "@sudoo/marked";
 
 export const MarkedMathMixinDeclaration: string = [
 
+    `declare function abs(value: number): number;`,
+
+    `declare function log(value: number): number;`,
+    `declare function log10(value: number): number;`,
+    `declare function log2(value: number): number;`,
+    `declare function log1p(value: number): number;`,
+
+    `declare function ceil(value: number): number;`,
+    `declare function floor(value: number): number;`,
+    `declare function round(value: number): number;`,
+
+    `declare function sin(value: number): number;`,
+    `declare function cos(value: number): number;`,
+    `declare function tan(value: number): number;`,
+
+    `declare function pow(value: number, power: number): number;`,
+    `declare function sqrt(value: number): number;`,
 ].join(New_Line_Character);
--- src/mixin.ts
+++ src/mixin.ts
@@ -6,6 +6,22 @@
 
 import { IMarkedMonacoManager, MarkedMonacoMixin } from "@sudoo/marked-monaco";
 
-export const markedMathMonacoMixin: MarkedMonacoMixin = (manager: IMarkedMonacoManager) => {
+export const createProvideMarkedMathMixin = (): MarkedMonacoMixin => {
 
+    return (manager: IMarkedMonacoManager) => {
+
+        const languageServer = manager.getLanguageServerDefaults();
+
+        return;
+    };
+};
+
+export const createInjectMarkedMathMixin = (): MarkedMonacoMixin => {
+
+    return (manager: IMarkedMonacoManager) => {
+
+        const languageServer = manager.getLanguageServerDefaults();
+
+        return;
+    };
 };
